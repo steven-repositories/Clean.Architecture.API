cae549a33610fe2a048fa6e2b7d12adf80f7c857
steven-repositories <stevenlestatan@gmail.com>
2024-10-04 04:08:34 +0800

    Added Clients as DbSet<Clients> in AppDbContext

--- TAN_10042024/Application/Data/AppDbContext.cs
+++ TAN_10042024/Application/Data/AppDbContext.cs
@@ -4,6 +4,7 @@ using TAN_10042024.Domain.Entities;
 namespace TAN_10042024.Application.Data {
     public class AppDbContext : DbContext {
         public DbSet<AuthorizationTokens> AuthorizationTokens { get; set; }
+        public DbSet<Clients> Clients { get; set; }
 
         public AppDbContext(DbContextOptions options) : base(options) { }
 
