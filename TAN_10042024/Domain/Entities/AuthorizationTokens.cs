5ba9f3215cd0d0ce367801bdb5d2cba6b4862bb1
steven-repositories <stevenlestatan@gmail.com>
2024-10-04 04:03:58 +0800

    Added foreign key in Authorization Tokens ClientId property

--- TAN_10042024/Domain/Entities/AuthorizationTokens.cs
+++ TAN_10042024/Domain/Entities/AuthorizationTokens.cs
@@ -1,6 +1,9 @@
-﻿namespace TAN_10042024.Domain.Entities {
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TAN_10042024.Domain.Entities {
     public class AuthorizationTokens : Entity {
         public Guid Token { get; set; }
+        [ForeignKey("Clients")]
         public int ClientId { get; set; }
     }
 }
