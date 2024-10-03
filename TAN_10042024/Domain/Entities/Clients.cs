9b63f76f46f987fdd773c542c9a98b4065c5d265
steven-repositories <stevenlestatan@gmail.com>
2024-10-04 04:03:28 +0800

    Added Clients entity

---
+++ TAN_10042024/Domain/Entities/Clients.cs
@@ -0,0 +1,5 @@
+﻿namespace TAN_10042024.Domain.Entities {
+    public class Clients : Entity {
+        public string Name { get; set; }
+    }
+}
---
+++ TAN_10042024/Migrations/20241003195958_AuthTokens To AuthorizationTokens.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TAN_10042024.Migrations
+{
+    /// <inheritdoc />
+    public partial class AuthTokensToAuthorizationTokens : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AuthTokens",
+                table: "AuthTokens");
+
+            migrationBuilder.RenameTable(
+                name: "AuthTokens",
+                newName: "AuthorizationTokens");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AuthTokens_Token",
+                table: "AuthorizationTokens",
+                newName: "IX_AuthorizationTokens_Token");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AuthorizationTokens",
+                table: "AuthorizationTokens",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AuthorizationTokens",
+                table: "AuthorizationTokens");
+
+            migrationBuilder.RenameTable(
+                name: "AuthorizationTokens",
+                newName: "AuthTokens");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AuthorizationTokens_Token",
+                table: "AuthTokens",
+                newName: "IX_AuthTokens_Token");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AuthTokens",
+                table: "AuthTokens",
+                column: "Id");
+        }
+    }
+}
