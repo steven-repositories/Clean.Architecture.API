187fc2f2fa8d534f16956c695c2ebf992b9ede4a
steven-repositories <stevenlestatan@gmail.com>
2024-10-04 04:09:42 +0800

    Added new migration for creating clients table

--- TAN_10042024/Migrations/20241003200849_AddClients.Designer.cs
+++ TAN_10042024/Migrations/20241003200849_AddClients.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TAN_10042024.Application.Data;
 
@@ -11,9 +12,11 @@ using TAN_10042024.Application.Data;
 namespace TAN_10042024.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241003200849_AddClients")]
+    partial class AddClients
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
