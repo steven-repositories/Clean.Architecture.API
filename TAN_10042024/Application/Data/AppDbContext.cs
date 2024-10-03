e127248f0e23b8f100ccb175e84a3bee5b03e33b
steven-repositories <stevenlestatan@gmail.com>
2024-10-04 02:08:08 +0800

    Added entity framework core package; Added AppDbContext class; Added AuthorizationTokens entity-class (ongoing); Added AppDbContext dependency injection for DbContextOptions upon build;
    * Added from the last commit before this exceptions class for future exception throws for error handlings;

---
+++ TAN_10042024/Application/Data/AppDbContext.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TAN_10042024.Application.Data {
+    public class AppDbContext : DbContext {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+    }
+}
--- TAN_10042024/Program.cs
+++ TAN_10042024/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TAN_10042024.Application.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<AppDbContext>((options) =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CC")));
 
 var app = builder.Build();
 
