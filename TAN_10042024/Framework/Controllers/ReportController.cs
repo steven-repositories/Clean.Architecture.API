a3e3d6fceb88630666dfc57516987c995d79a60f
steven-repositories <stevenlestatan@gmail.com>
2024-10-04 19:01:08 +0800

    Added ReportController, preparation for reporting purposes

---
+++ TAN_10042024/Framework/Controllers/ReportController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TAN_10042024.Framework.Controllers {
+    [Route("api/report")]
+    [ApiController, Produces("application/json")]
+    public class ReportController : ControllerBase {
+        public ReportController() {
+
+        }
+    }
+}
