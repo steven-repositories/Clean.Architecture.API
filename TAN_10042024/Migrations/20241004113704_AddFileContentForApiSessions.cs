34925ecd468c423bf32f0ce71551e7415eb984b5
steven-repositories <stevenlestatan@gmail.com>
2024-10-04 19:37:26 +0800

    Added new migration for adding new column for ApiSessions named FileContent

---
+++ TAN_10042024/Migrations/20241004113704_AddFileContentForApiSessions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TAN_10042024.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFileContentForApiSessions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FileContent",
+                table: "ApiSessions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileContent",
+                table: "ApiSessions");
+        }
+    }
+}
