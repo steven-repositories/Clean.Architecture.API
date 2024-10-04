b0b87ca6086a9070dee23763dc25a5f3e6b5dfe3
steven-repositories <stevenlestatan@gmail.com>
2024-10-04 16:14:12 +0800

    Added ApiSessions entity for file tracking and reporting

---
+++ TAN_10042024/Domain/Entities/ApiSessions.cs
@@ -0,0 +1,7 @@
+﻿namespace TAN_10042024.Domain.Entities {
+    public class ApiSessions : Entity {
+        public required string Method { get; set; }
+        public required string URL { get; set; }
+        public required string FileName { get; set; }
+    }
+}
