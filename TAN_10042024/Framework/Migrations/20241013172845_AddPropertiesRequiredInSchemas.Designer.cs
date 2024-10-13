e06e83a7d9b6a096e49b01b45b7896c36e3aa515
steven-repositories <stevenlestatan@gmail.com>
2024-10-14 01:29:26 +0800

    Added new migration for changing the column names for Files table - migration AddPropertiesRequiredInSchemas;

--- TAN_10042024/Framework/Migrations/20241013172845_AddPropertiesRequiredInSchemas.Designer.cs
+++ TAN_10042024/Framework/Migrations/20241013172845_AddPropertiesRequiredInSchemas.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TAN_10042024.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using TAN_10042024.Infrastructure.Data;
 namespace TAN_10042024.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241013172845_AddPropertiesRequiredInSchemas")]
+    partial class AddPropertiesRequiredInSchemas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TAN_10042024/Framework/Migrations/20241013172845_AddPropertiesRequiredInSchemas.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TAN_10042024.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPropertiesRequiredInSchemas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FileName",
+                table: "Files",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "FileContent",
+                table: "Files",
+                newName: "Content");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Files",
+                newName: "FileName");
+
+            migrationBuilder.RenameColumn(
+                name: "Content",
+                table: "Files",
+                newName: "FileContent");
+        }
+    }
+}
