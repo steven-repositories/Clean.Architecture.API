2ec546a708ac52c0f4694bba4229e9f9c392e7fd
steven-repositories <stevenlestatan@gmail.com>
2024-10-16 01:43:23 +0800

    Enclosed the serviceScope and dbContext variables inside the try block

--- TAN_10042024/Application/Services/MigrationService.cs
+++ TAN_10042024/Application/Services/MigrationService.cs
@@ -7,16 +7,16 @@ using static TAN_10042024.Application.Utilities.Exceptions;
 namespace TAN_10042024.Application.Services {
     public class MigrationService : IMigration {
         public void ExecuteMigrations(IHost host) {
-            var serviceScope = host
+            try {
+                var serviceScope = host
                 .Services
                 .CreateScope();
 
-            var dbContext = serviceScope
-                .ServiceProvider
-                .GetRequiredService<AppDbContext>()
-                .Database;
+                var dbContext = serviceScope
+                    .ServiceProvider
+                    .GetRequiredService<AppDbContext>()
+                    .Database;
 
-            try {
                 dbContext.Migrate();
             } catch (MigrationException e) {
                 throw new MigrationException("Something went wrong during the execution of the migration files : {0}"
