ddda6721ebd65ad31198f1f9124852e1b59ec45a
steven-repositories <stevenlestatan@gmail.com>
2025-03-01 04:38:57 +0800

    Changes in gitignore to make the xUnit project not ignored

---
+++ Clean.Architecture.API.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+using Xunit;
+
+namespace Clean.Architecture.API.Tests {
+    public class UnitTest1 {
+        [Fact]
+        public void Test1() {
+            Assert.True(true);
+        }
+    }
+}
